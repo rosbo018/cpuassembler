c08de07bf0527ce6fd85b7083a23f1c7523dc5d2
rosbo018 <rosbo018@uottawa.ca>
2017-12-08 17:08:01 -0500

    design

---
+++ assembler.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#inlcude <boost>
+
+std::vector<int> parseDB(std::string dbStrings[]){
+  std::vector<int> useMemory = new std::vector<int>();
+  for(std::string line : dbStrings ){
+    std::
+  }
+} 
+
+
+int main(int argc, string argv[]){
+  std::string line;
+  std::ifstream infile()
+}
